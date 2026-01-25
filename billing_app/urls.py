b20e65ce63f16fd8e48aa635eb3d9371ef9509a1
Midhuna M B <145039182+Midhu02@users.noreply.github.com>
2026-01-25 12:40:04 +0530

    Update urls.py

--- billing_app/urls.py
+++ billing_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('billing/', views.billing_page, name='billing'),
     path('inventory/', views.inventory_page, name='inventory'),
